3cf753669487f10a8eb320cf5db11fc842d231bb
Max Luz Carvalho <128647587+maxluzcarvalho@users.noreply.github.com>
2023-05-11 10:21:32 +0200

    resolução do exercicio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+interface Matriculavel {
+    fun matricular(usuario: Usuario)
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional>) : Matriculavel {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    override fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
+    }
+}
+
+open class Curso(val nome: String, val nivel: Nivel, val duracao: Int) : Matriculavel {
+
+    val inscritos = mutableListOf<Usuario>()
+
+    override fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado no curso $nome.")
+    }
+}
+
+class CursoPresencial(nome: String, nivel: Nivel, duracao: Int, val local: String) : Curso(nome, nivel, duracao) {
+    fun iniciar() {
+        println("Iniciando curso presencial $nome no local $local.")
+    }
+}
+
+class CursoOnline(nome: String, nivel: Nivel, duracao: Int, val url: String) : Curso(nome, nivel, duracao) {
+    fun reproduzir() {
+        println("Reproduzindo curso online $nome na URL $url.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Max")
+    val usuario2 = Usuario("Vanessa")
+
+    val conteudo1 = ConteudoEducacional("Kotlin Básico")
+    val conteudo2 = ConteudoEducacional("Kotlin Intermediário")
+    val conteudo3 = ConteudoEducacional("Kotlin Avançado")
+
+    val formacao = Formacao("Formação Kotlin", mutableListOf(conteudo1, conteudo2, conteudo3))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    val cursoPresencial = CursoPresencial("Kotlin para Android", Nivel.INTERMEDIARIO, 40, "São Paulo")
+    cursoPresencial.matricular(usuario1)
+    cursoPresencial.iniciar()
+
+    val cursoOnline = CursoOnline("Kotlin para Web", Nivel.AVANCADO, 60, "https://www.dio.me/en")
+    cursoOnline.matricular(usuario2)
+    cursoOnline.reproduzir()
 }
